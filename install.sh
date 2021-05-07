162817ede856fd33c3c33b6bc1413efa8279e214
kevin Lyon <69926397+kevinLyon@users.noreply.github.com>
2021-05-07 20:10:56 -0300

    correcting the installation order

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cp Bloody.flf /usr/share/figlet #Banner
 apt-get update
 apt-get install lolcat -y #Dependency
 apt-get install figlet -y #Dependency
+cp Bloody.flf /usr/share/figlet #Banner
 pip3 install dnspython
 echo "DONE :)"
